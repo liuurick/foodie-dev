467a853520dabb647e5c9c244a31aaf0254b1902
liuurick <liuurick@gmail.com>
2021-09-19 22:56:12 +0800

    feat: 引入swagger

--- foodie-dev-api/src/main/java/com/liubin/controller/HelloController.java
+++ foodie-dev-api/src/main/java/com/liubin/controller/HelloController.java
@@ -1,5 +1,7 @@
 package com.liubin.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,11 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * @author liubin
  */
+@Api(tags = "接口测试")
 @RestController
 public class HelloController {
 
     private final static Logger logger = LoggerFactory.getLogger(HelloController.class);
 
+    @ApiOperation(value = "hello")
     @GetMapping("/hello")
     public String hello() {
 
