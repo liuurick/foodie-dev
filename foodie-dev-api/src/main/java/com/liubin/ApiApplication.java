def0ab54c7db90f900dee01e6dbcd3d61254bd6c
liubin <liubin.lb@elecwatt.com>
2021-11-22 09:11:42 +0800

    feat: 日志美化

--- foodie-dev-api/src/main/java/com/liubin/ApiApplication.java
+++ foodie-dev-api/src/main/java/com/liubin/ApiApplication.java
@@ -1,26 +1,33 @@
 package com.liubin;
 
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 /**
  * @author liubin
  */
 @SpringBootApplication
+@Slf4j
 public class ApiApplication {
-    private static final Logger LOG = LoggerFactory.getLogger(ApiApplication.class);
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws UnknownHostException {
 
-        SpringApplication app = new SpringApplication(ApiApplication.class);
-        Environment env = app.run(args).getEnvironment();
-        LOG.info("启动成功！！");
-        LOG.info("启动地址为: \thttp://127.0.0.1:{}", env.getProperty("server.port"));
+        Environment env = new SpringApplication(ApiApplication.class).run(args).getEnvironment();
+        String envPort = env.getProperty("server.port");
+        String envContext = env.getProperty("server.servlet.context-path");
+        String port = envPort == null ? "8080" : envPort;
+        String context = envContext == null ? "" : envContext;
+        String path = port + "" + context + "/doc.html";
+        String druidPath = port + "" + context + "/druid";
+        log.info("Access URLs:\n----------------------------------------------------------\n\t"
+                        + "Local: \t\thttp://127.0.0.1:{}\n\t" + "External: \thttp://{}:{}\n\t"
+                        + "----------------------------------------------------------", path,
+                InetAddress.getLocalHost().getHostAddress(), path, druidPath);
     }
-
 }
