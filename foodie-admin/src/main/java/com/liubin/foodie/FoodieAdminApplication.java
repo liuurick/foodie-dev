4e5a691aa51a3404d8d0a57b498bc4e8616ebd27
liubin <liubin.lb@elecwatt.com>
2021-12-10 18:00:28 +0800

    feat: 更新

--- foodie-admin/src/main/java/com/liubin/foodie/FoodieAdminApplication.java
+++ foodie-admin/src/main/java/com/liubin/foodie/FoodieAdminApplication.java
@@ -3,7 +3,6 @@ package com.liubin.foodie;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
 
 import java.net.InetAddress;
