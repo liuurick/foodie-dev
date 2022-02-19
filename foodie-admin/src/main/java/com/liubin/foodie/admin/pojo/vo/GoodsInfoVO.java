5ccd04aa21675a60c41d480895cb49a3331dad66
liuurick <liuurick@gmail.com>
2022-02-19 22:37:48 +0800

    fix: update

--- foodie-admin/src/main/java/com/liubin/foodie/admin/pojo/vo/GoodsInfoVO.java
+++ foodie-admin/src/main/java/com/liubin/foodie/admin/pojo/vo/GoodsInfoVO.java
@@ -1,7 +1,7 @@
 package com.liubin.foodie.admin.pojo.vo;
 
 import com.liubin.foodie.admin.pojo.Goods;
-import com.liubin.foodie.admin.pojo.ItemsImg;
+import com.liubin.foodie.admin.pojo.GoodsImg;
 import com.liubin.foodie.admin.pojo.GoodsParam;
 import com.liubin.foodie.admin.pojo.GoodsSpec;
 import lombok.Data;
@@ -12,10 +12,10 @@ import java.util.List;
  * 商品详情VO
  */
 @Data
-public class ItemInfoVO {
+public class GoodsInfoVO {
 
-    private Goods item;
-    private List<ItemsImg> itemImgList;
-    private List<GoodsSpec> itemSpecList;
-    private GoodsParam itemParams;
+    private Goods goods;
+    private List<GoodsImg> goodsImgList;
+    private List<GoodsSpec> goodsSpecList;
+    private GoodsParam goodsParams;
 }
