379006fad4c6b66f46f890b23381fc5c08f59528
liuurick <liuurick@gmail.com>
2022-02-19 19:29:13 +0800

    fix: 登录接口

--- foodie-admin/src/main/java/com/liubin/foodie/admin/controller/UserController.java
+++ foodie-admin/src/main/java/com/liubin/foodie/admin/controller/UserController.java
@@ -2,6 +2,9 @@ package com.liubin.foodie.admin.controller;
 
 import com.liubin.common.api.CommonResult;
 import com.liubin.common.config.Summary;
+import com.liubin.common.utils.CookieUtils;
+import com.liubin.common.utils.JsonUtils;
+import com.liubin.common.utils.MD5Utils;
 import com.liubin.foodie.admin.pojo.Users;
 import com.liubin.foodie.admin.pojo.bo.UserBO;
 import com.liubin.foodie.admin.service.UserService;
@@ -26,6 +29,25 @@ public class UserController {
     @Resource
     private UserService userService;
 
+    @ApiOperation(value = "用户名是否存在", notes = "用户名是否存在", httpMethod = "GET")
+    @GetMapping("/usernameIsExist")
+    public CommonResult usernameIsExist(@RequestParam String username) {
+
+        // 1.判断用户名不能为空
+        if (StringUtils.isBlank(username)) {
+            return CommonResult.failed("用户名不能为空");
+        }
+
+        // 2.查找注册的用户名是否存在
+        boolean isExist = userService.queryUsernameIsExist(username);
+        if (isExist) {
+            return CommonResult.failed("用户名已经存在");
+        }
+
+        // 3.请求成功，用户名没有重复
+        return CommonResult.success();
+    }
+
     @ApiOperation(value = "用户注册", notes = "用户注册", httpMethod = "POST")
     @PostMapping("/register")
     public CommonResult<Users> register(@RequestBody @Valid UserBO userBO,
@@ -38,9 +60,15 @@ public class UserController {
         if (!password.equals(confirmPwd)){
             return CommonResult.failed("密码和确认密码不相同");
         }
+
+        // 密码长度不能少于6位
+        if (password.length() < 6) {
+            return CommonResult.failed("密码长度不能少于6");
+        }
+
         // 判断用户名和密码必须不为空
-        if (StringUtils.isBlank(username) ||
-                StringUtils.isBlank(password) ||
+        if (StringUtils.isBlank(username) &&
+                StringUtils.isBlank(password) &&
                 StringUtils.isBlank(confirmPwd)) {
             return CommonResult.failed("用户名或密码不能为空");
         }
@@ -51,5 +79,68 @@ public class UserController {
         }
         return CommonResult.success(userService.createUser(userBO));
     }
+
+    @ApiOperation(value = "用户登录", notes = "用户登录", httpMethod = "POST")
+    @PostMapping("/login")
+    public CommonResult login(@RequestBody UserBO userBO,
+                                 HttpServletRequest request,
+                                 HttpServletResponse response) throws Exception {
+
+        String username = userBO.getUsername();
+        String password = userBO.getPassword();
+
+        // 0. 判断用户名和密码必须不为空
+        if (StringUtils.isBlank(username) ||
+                StringUtils.isBlank(password)) {
+            return CommonResult.failed("用户名或密码不能为空");
+        }
+
+        // 1. 实现登录
+        Users userResult = userService.queryUserForLogin(username,
+                MD5Utils.getMD5Str(password));
+
+        if (userResult == null) {
+            return CommonResult.failed("用户名或密码不正确");
+        }
+
+        userResult = setNullProperty(userResult);
+
+        CookieUtils.setCookie(request, response, "user",
+                JsonUtils.objectToJson(userResult), true);
+
+
+        // TODO 生成用户token，存入redis会话
+        // TODO 同步购物车数据
+
+        return CommonResult.success(userResult);
+    }
+
+    @ApiOperation(value = "用户退出登录", notes = "用户退出登录", httpMethod = "POST")
+    @PostMapping("/logout")
+    public CommonResult logout(@RequestParam String userId,
+                                  HttpServletRequest request,
+                                  HttpServletResponse response) {
+
+        // 清除用户的相关信息的cookie
+        CookieUtils.deleteCookie(request, response, "user");
+
+        // TODO 用户退出登录，需要清空购物车
+        // TODO 分布式会话中需要清除用户数据
+
+        return CommonResult.success();
+    }
+
+
+    private Users setNullProperty(Users userResult) {
+        userResult.setPassword(null);
+        userResult.setMobile(null);
+        userResult.setEmail(null);
+        userResult.setGmtCreate(null);
+        userResult.setGmtUpdate(null);
+        userResult.setBirthday(null);
+        return userResult;
+    }
+
+
 
 }
